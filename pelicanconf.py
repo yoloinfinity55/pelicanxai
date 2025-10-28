3fb8e864c11cf4d4b6250d4a32e1a61239acad48
yoloinfinity55 <yoloinfinity55@gmail.com>
2025-10-28 18:14:03 -0400

    updated

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,5 +31,7 @@ SOCIAL = (
 
 DEFAULT_PAGINATION = 10
 
+THEME = "themes/custom"
+
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./themes/custom/templates/**/*.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
